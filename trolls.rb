bfff8b19594b947b7a7b19535f490547db620926
manouille003 <lemoinejulia88@gmail.com>
2025-03-14 20:24:44 +0100

    apero go

---
+++ trolls.rb
@@ -0,0 +1,8 @@
+puts "A common way to deal with this situation is to remove all the vowels form the troll's comments, neutralizing the threat."
+
+troll_comment = gets.chomp
+
+attack = troll_comment.delete("AEIOUaeiou")
+
+puts attack
+
